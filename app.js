64d01477b0f085f021b764c48a44113ba2a60e5f
Henrik <githenrik@gmail.com>
2021-07-06 10:50:59 +0200

    Commit fra VS Code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from meeee 3x AGAIN!'));
+app.get('/', (req, res) => res.send('Hello from VS controlled!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
