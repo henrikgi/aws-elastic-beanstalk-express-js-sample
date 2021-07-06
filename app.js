ea40ea6d189868fea6075fcbf02ca083bdcdbe2a
Henrik <githenrik@gmail.com>
2021-07-06 10:59:29 +0200

    Manual review added in pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from VS controlled!'));
+app.get('/', (req, res) => res.send('Hello manually reviewed from VS controlled!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
