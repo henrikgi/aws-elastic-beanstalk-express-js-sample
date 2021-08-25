f4925f347f20905e4b0e618ef4d7a5b0b5f8f1f8
Henrik <githenrik@gmail.com>
2021-08-25 19:31:02 +0200

    Reviewable at 19:30

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Henrik 25.8 18:33!'));
+app.get('/', (req, res) => res.send('Henrik reviewable 25.8 19:30!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
