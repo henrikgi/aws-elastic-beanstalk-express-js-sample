75f56295d3e098a8045603a19e07e79d5515736b
Henrik <githenrik@gmail.com>
2021-08-25 18:38:07 +0200

    1833 i dag

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello manually reviewed from VS controlled!'));
+app.get('/', (req, res) => res.send('Henrik 25.8 18:33!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
